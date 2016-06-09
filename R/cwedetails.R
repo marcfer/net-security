50c45e18aa4473f3e3f205598b6c2858a5051302
Cristian Barrientos Beltran <barri@MacBook-Pro-de-Cristian.local>
2016-06-09 19:52:08 +0200

    Parse XML to DF, first commit OK

--- R/cwedetails.R
+++ R/cwedetails.R
@@ -4,12 +4,12 @@
 library(XML)
 library(gtools) #rbind.data.frame(xpath1[[1]], xpath1[[2]], ...) --- do.call(rbind.data.frame, xpath1)
 parseXMLtoDF <- function(xml) {
-  df <- xmlTreeParse("/Users/barri/Desktop/Universidad/Master/3. Data Driven Security/Rproject/net-security/data/cwec_v2.9.xml", useInternalNodes = TRUE)
-  rootNode <- xmlRoot(df) #xmlName(rootNode) = Weakness_Catalog
+  xmlTree <- xmlTreeParse("/Users/barri/Desktop/Universidad/Master/3. Data Driven Security/Rproject/net-security/data/cwec_v2.9.xml", useInternalNodes = TRUE)
+  rootNode <- xmlRoot(xmlTree) #xmlName(rootNode) = Weakness_Catalog
   weaknessesNode <- rootNode[[3]] #weaknessesNode = Weaknesses
-  xpath1 <- xpathApply(weaknessesNode, "//Weakness[@ID='102']", xmlAttrs)
-  xpath2 <- xpathApply(weaknessesNode, "//Weakness[@ID='102']") 
-  
+  xpath <- xpathApply(weaknessesNode, "//Weakness[@ID<'110']", xmlAttrs)
+  dataFrame <- do.call(rbind.data.frame, xpath)
+  names(dataFrame) <- c("ID", "Name", "Weakness_Abstraction", "Status")
 }
 
 # =================
