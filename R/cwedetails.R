69e0d1e6bde56102421345aaaf4e4df5296abadc
Cristian Barrientos Beltran <barri@MacBook-Pro-de-Cristian.local>
2016-06-09 21:13:51 +0200

    Parse XML to DF, second commit

--- R/cwedetails.R
+++ R/cwedetails.R
@@ -2,14 +2,26 @@
 # Public Functions
 # ================
 library(XML)
-library(gtools) #rbind.data.frame(xpath1[[1]], xpath1[[2]], ...) --- do.call(rbind.data.frame, xpath1)
-parseXMLtoDF <- function(xml) {
-  xmlTree <- xmlTreeParse("/Users/barri/Desktop/Universidad/Master/3. Data Driven Security/Rproject/net-security/data/cwec_v2.9.xml", useInternalNodes = TRUE)
+#library(gtools) #rbind.data.frame(xpath1[[1]], xpath1[[2]], ...) --- do.call(rbind.data.frame, xpath1)
+parseXMLtoDF <- function() {
+  xmlTree <- xmlTreeParse("./data/cwec_v2.9.xml", useInternalNodes = TRUE)
   rootNode <- xmlRoot(xmlTree) #xmlName(rootNode) = Weakness_Catalog
   weaknessesNode <- rootNode[[3]] #weaknessesNode = Weaknesses
   xpath <- xpathApply(weaknessesNode, "//Weakness[@ID<'110']", xmlAttrs)
   dataFrame <- do.call(rbind.data.frame, xpath)
   names(dataFrame) <- c("ID", "Name", "Weakness_Abstraction", "Status")
+  
+  xpath2 <- xpathApply(weaknessesNode, "//Weakness[@ID<'110']/Description")
+  dataFrame2 <- xmlToDataFrame(xpath2)
+  
+  dataFrame3 <- merge(x = dataFrame, y = dataFrame2, by = NULL)
+  
+  #Getting CVEs associated (one column data frame with CVEs Associated)
+  xpath3 <- xpathApply(weaknessesNode, "//Weakness[@ID<'110']")
+  dataFrame4 <- as.data.frame(xmlToDataFrame(xpath3)$Observed_Examples)
+  names(dataFrame4) <- c("Associated CVEs")
+  
+  
 }
 
 # =================
