84cb716730abbd2e73602b2d261036ef1a2e43fc
Marc Fernández Antolín <marc@Marcs-MacBook-Air.local>
2016-06-09 21:10:30 +0200

    getCWEName and getCVElistfromText

--- R/cwedetails.R
+++ R/cwedetails.R
@@ -2,9 +2,16 @@
 # Public Functions
 # ================
 library(XML)
-library(gtools) #rbind.data.frame(xpath1[[1]], xpath1[[2]], ...) --- do.call(rbind.data.frame, xpath1)
-parseXMLtoDF <- function(xml) {
-  xmlTree <- xmlTreeParse("/Users/barri/Desktop/Universidad/Master/3. Data Driven Security/Rproject/net-security/data/cwec_v2.9.xml", useInternalNodes = TRUE)
+#library(gtools) #rbind.data.frame(xpath1[[1]], xpath1[[2]], ...) --- do.call(rbind.data.frame, xpath1)
+
+#' Function that loads the CWE file into a data frame and returns this data frame
+#'
+#' @return dataframe with the cwe info
+#' @export
+#'
+#' @examples
+parseXMLtoDF <- function() {
+  xmlTree <- xmlTreeParse("./data/cwec_v2.9.xml", useInternalNodes = TRUE)
   rootNode <- xmlRoot(xmlTree) #xmlName(rootNode) = Weakness_Catalog
   weaknessesNode <- rootNode[[3]] #weaknessesNode = Weaknesses
   xpath <- xpathApply(weaknessesNode, "//Weakness[@ID<'110']", xmlAttrs)
@@ -12,6 +19,35 @@ parseXMLtoDF <- function(xml) {
   names(dataFrame) <- c("ID", "Name", "Weakness_Abstraction", "Status")
 }
 
+#' Title
+#'
+#' @param id the ID of the CWE as a number
+#'
+#' @return the Name of the CWE
+#' @export
+#'
+#' @examples getCWENameByID(94) getCWENameByID(102)
+getCWENameByID <- function(id) {
+  cweDF <- parseXMLtoDF()
+  cweDF <- subset(cweDF, ID==id)
+  res <- as.character(df$Name)
+  return(res)
+}
+
+
+
 # =================
 # Private Functions
 # =================
+
+#' Returns a list of CVE from an input string
+#'
+#' @param string the text from where the CVEs want to be selected
+#'
+#' @return a list with the CVEs present in the text "string"
+#' @export
+#'
+#' @examples getCVElistFromText(CVE-2009-1936chain: library CVE-2005-3335)
+getCVElistFromText <- function(string) {
+  return(regmatches(string, gregexpr("[A-Z]{3}-[0-9]{4}-[0-9]{3,10}", string)))
+}
