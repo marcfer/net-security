2c5bf334cbedb7cc39d6ef1488820b5cfc6df93a
Humbert Costas <hcostas@ackcent.com>
2016-06-12 22:31:34 +0200

    - Changed function name CWE instead of XML. - Removed XPATH contidion - Changed some factors to character - Solved error in getCWENameByID with internal variable names

--- R/cwedetails.R
+++ R/cwedetails.R
@@ -2,29 +2,27 @@
 # Public Functions
 # ================
 
-#library(gtools) #rbind.data.frame(xpath1[[1]], xpath1[[2]], ...) --- do.call(rbind.data.frame, xpath1)
 #' Function that loads the CWE file into a data frame and returns this data frame
 #'
 #' @return dataframe with the cwe info
 #' @export
 #'
 #' @examples
-
-parseXMLtoDF <- function() {
+parseCWEtoDF <- function() {
   xmlTree <- XML::xmlTreeParse("./data/cwec_v2.9.xml", useInternalNodes = TRUE)
   rootNode <- XML::xmlRoot(xmlTree) #xmlName(rootNode) = Weakness_Catalog
   weaknessesNode <- rootNode[[3]] #weaknessesNode = Weaknesses
-  xpath <- XML::xpathApply(weaknessesNode, "//Weakness[@ID<'110']", XML::xmlAttrs)
+  xpath <- XML::xpathApply(weaknessesNode, "//Weakness", XML::xmlAttrs)
   dataFrame <- do.call(rbind.data.frame, xpath)
   names(dataFrame) <- c("ID", "Name", "Weakness_Abstraction", "Status")
 
-  xpath2 <- XML::xpathApply(weaknessesNode, "//Weakness[@ID<'110']/Description")
+  xpath2 <- XML::xpathApply(weaknessesNode, "//Weakness/Description")
   dataFrame2 <- XML::xmlToDataFrame(xpath2)
 
   dataFrame3 <- data.frame(dataFrame, dataFrame2)
 
   #Getting CVEs associated (one column data frame with CVEs Associated)
-  xpath3 <- XML::xpathApply(weaknessesNode, "//Weakness[@ID<'110']")
+  xpath3 <- XML::xpathApply(weaknessesNode, "//Weakness")
   dataFrame4 <- as.data.frame(XML::xmlToDataFrame(xpath3)$Observed_Examples)
   names(dataFrame4) <- c("Associated_CVEs")
   dataFrame4$Associated_CVEs <- as.character(dataFrame4$Associated_CVEs)
@@ -34,10 +32,13 @@ parseXMLtoDF <- function() {
     # do stuff with row
     dataFrame4[i,1] <- jsonlite::toJSON(getCVElistFromText(row))
   }
-  dataFrame5 <- data.frame(dataFrame3, dataFrame4)
+  dataFrame5 <- data.frame(dataFrame3, dataFrame4, stringsAsFactors = FALSE)
+  dataFrame5$ID <- as.character(dataFrame5$ID)
+  dataFrame5$Name <- as.character(dataFrame5$Name)
 
   return(dataFrame5)
 }
+
 #' Title
 #'
 #' @param id the ID of the CWE as a number
@@ -47,9 +48,9 @@ parseXMLtoDF <- function() {
 #'
 #' @examples getCWENameByID(94) getCWENameByID(102)
 getCWENameByID <- function(id) {
-  cweDF <- parseXMLtoDF()
+  cweDF <- parseCWEtoDF()
   cweDF <- subset(cweDF, ID == id)
-  res <- as.character(df$Name)
+  res <- as.character(cweDF$Name)
   return(res)
 }
 
